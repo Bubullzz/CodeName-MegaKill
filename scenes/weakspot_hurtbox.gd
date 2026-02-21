5f4b4c727d5e9a53c5ec8026aeb77d06102e1bec
32cls <nicolas.load@gmail.com>
2026-02-21 19:22:59 +0100

    add: weakspots

--- scenes/weakspot_hurtbox.gd
+++ scenes/weakspot_hurtbox.gd
@@ -1,7 +1,8 @@
 extends CustomHurtbox
-
+class_name WeakspotHurtbox
 
 func hit(weapon: Weapon) -> void:
+	print("weakspot hit")
 	goon.weakspot_hit(weapon)
 	
 # Called when the node enters the scene tree for the first time.
